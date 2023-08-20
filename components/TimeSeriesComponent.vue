824eed8334e856f4841c434b29aad41984c6637d
Sujit Joshi <joshi.sujit020386@gmail.com>
2023-08-20 17:10:38 -0400

    Prints grid layout on "Save Grid Layout" button click to console.

--- components/TimeSeriesComponent.vue
+++ components/TimeSeriesComponent.vue
@@ -1,6 +1,6 @@
 <template>
 		<div class="q-pa-md">
-				<div class="grid-stack y-grid-stack">
+				<div :class="{'grid-stack': true, 'y-grid-stack': true}">
 						<div v-for="(widget, index) in items" :key="index"
 						     :gs-x="widget.x" :gs-y="widget.y"
 						     :gs-w="widget.w" :gs-h="widget.h"
@@ -8,33 +8,43 @@
 								<div class="grid-stack-item-content c-grid-stack-item-content">{{ widget.content }}</div>
 						</div>
 				</div>
+				<q-btn @click="saveGrid" label="Save Grid Layout"/>
 		</div>
 </template>
 
 <script setup lang="ts">
+import {GridStack} from 'gridstack';
 
-import { GridStack } from 'gridstack';
-let grid: GridStack;
-import {GridStackWidget} from "gridstack/dist/types";
-import {Ref} from "@vue/reactivity";
-
-let items: Ref<GridStackWidget[]> = ref([
+let items = ref([
 		{x: 0, y: 0, w: 2, h: 2, content: '1'},
 		{x: 0, y: 4, w: 4, h: 4, content: '2'},
-		{x: 8, y: 8, w: 4, h: 4, content: '3'}
+		{x: 0, y: 6, w: 4, h: 4, content: '3'},
+		{x: 0, y: 6, w: 4, h: 4, content: '4'},
+		{x: 0, y: 6, w: 4, h: 4, content: '5'}
 ]);
 
+let grid: GridStack;
+
 onMounted(() => {
+		// No need to read return value of init() if you are not going to programmatically change the grid.
 		grid = GridStack.init({
 				cellHeight: '40px',
 				minRow: 9,
 				alwaysShowResizeHandle: true,
 		});
 });
+
+const saveGrid = () => {
+		const widgets = grid.save(false)
+		console.log(widgets)
+}
+
 </script>
 
+<!--PS: Try making this a scoped style. You'll notice that resize handles disappear.-->
 <style>
 @import 'gridstack/dist/gridstack.css';
+
 .y-grid-stack {
 		background: lightgoldenrodyellow;
 }
