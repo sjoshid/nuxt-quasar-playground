01e6551047e322275fe24e4d9b75790dd57aa3ed
Sujit Joshi <joshi.sujit020386@gmail.com>
2023-09-16 15:17:10 -0400

    Adding Highcharts-Vue as a Nuxt plugin.

--- components/OMetric.vue
+++ components/OMetric.vue
@@ -1,14 +1,26 @@
 <script lang="ts" setup>
 import {GridStackWidget} from "gridstack/dist/types";
+const highcharts = useNuxtApp().vueApp.component('highcharts')
 
 defineProps<{ widget: GridStackWidget }>()
+const chartOptions = ref({
+		title: "Sujit title",
+		series: [
+				{
+						data: [1, 0, 3],
+				}
+		],
+
+});
 </script>
 
 <template>
 		<div :gs-x="widget.x" :gs-y="widget.y"
 		     :gs-w="widget.w" :gs-h="widget.h"
 		     class="grid-stack-item">
-				<div class="grid-stack-item-content c-grid-stack-item-content">{{ widget.content }}</div>
+				<div class="grid-stack-item-content c-grid-stack-item-content">
+						<highcharts :options="chartOptions"/>
+				</div>
 		</div>
 </template>
 
