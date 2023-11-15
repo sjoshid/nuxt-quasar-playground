6044ae0e9f6e74bc23ca437b47223976a66e5d3f
Sujit Joshi <joshi.sujit020386@gmail.com>
2023-11-15 17:41:12 -0500

    Fixing start time with ZonedDateTime

--- components/ODateTimePicker.vue
+++ components/ODateTimePicker.vue
@@ -5,27 +5,27 @@
                 <q-icon class="cursor-pointer" name="event">
                     <q-popup-proxy cover transition-hide="scale" transition-show="scale">
                         <q-date :model-value="startDateTime.format(usDateFormatter)" :mask="quasarFormat"
-                                @update:model-value="nv => startDateTime.value = ZonedDateTime.parse(nv, usDateFormatter, )">
+                                @update:model-value="nv => startDateTime = ZonedDateTime.of(LocalDateTime.parse(nv, usDateFormatter), ZoneId.of('UTC'))">
                             <div class="row items-center justify-end">
-                                <q-btn v-close-popup color="primary" flat label="Close" />
+                                <q-btn v-close-popup color="primary" flat label="Close"/>
                             </div>
                         </q-date>
                     </q-popup-proxy>
                 </q-icon>
             </template>
 
-            <!-- <template v-slot:append>
+             <template v-slot:append>
                 <q-icon class="cursor-pointer" name="access_time">
                     <q-popup-proxy cover transition-hide="scale" transition-show="scale">
                         <q-time :model-value="startDateTime.format(usDateFormatter)" format24h :mask="quasarFormat"
-                            @update:model-value="nv => $emit('update:startDateTime', ZonedDateTime.parse(nv, usDateFormatter))">
+                            @update:model-value="nv => startDateTime = ZonedDateTime.of(LocalDateTime.parse(nv, usDateFormatter), ZoneId.of('UTC'))">
                             <div class="row items-center justify-end">
                                 <q-btn v-close-popup color="primary" flat label="Close" />
                             </div>
                         </q-time>
                     </q-popup-proxy>
                 </q-icon>
-            </template> -->
+            </template>
         </q-input>
         <!-- <q-input :model-value="endDateTime.format(usDateFormatter)" dense filled label="To" stack-label>
             <template v-slot:prepend>
@@ -58,7 +58,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ZonedDateTime } from "@js-joda/core";
+import {LocalDateTime, ZonedDateTime, ZoneId} from "@js-joda/core";
 
 const props = defineProps<{
     startDateTime: Ref<ZonedDateTime>,
